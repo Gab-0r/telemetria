a0a4567c1594e47bb6a4f7e1c8499e51942c8eb8
Gabriel <jgabriel.orozco@udea.edu.co>
2022-08-24 09:35:44 -0500

    Se añaden archivo de pines y datasheet.

--- telemetria.cpp
+++ telemetria.cpp
@@ -5,48 +5,5 @@
 #include "sd_manager.h"
 
 int main() {
-
-    FRESULT fr;
-    FATFS fs;
-    FIL fil;
-    int ret;
-    char buf[100];
-    char filename[] = "sdManager_test.txt";
-
-    //Inicializar seriales
-    stdio_init_all();
-
-    sleep_ms(5000);
-
-    // Inicializar tarjeta SD
-    initialize_sd();
-
-    //Montar el volumen
-    mount_drive(fr, &fs);
-
-    //Abrir archivo para escribir
-    sd_openfileW(fr, &fil, filename);
-
-    //Escribiendo en el archivo
-    sd_writefile(ret, &fil, "PRUEBITAAAAAAA");
-
-    //Cerrar el archivo
-    sd_closefile(fr, &fil);
-
-    //Abrir el archivo en modo lectura
-    sd_openfileR(fr, &fil, filename);
-
-    //Imprimir en serial lo que se lee del archivo
-    sd_printfile(buf, &fil);
-
-    //Cerrar el archivo
-    sd_closefile(fr, &fil);
-
-    //Desmontar dispositivo
-    f_unmount("0:");
-
-    // Loop forever doing nothing
-    while (true) {
-        sleep_ms(1000);
-    }
+    
 }
