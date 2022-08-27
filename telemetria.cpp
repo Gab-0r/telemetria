a836aacfcc14249f6e169e559eda5e400204528a
Gabriel <jgabriel.orozco@udea.edu.co>
2022-08-27 09:34:46 -0500

    Se escriben los datos del sensor en SD.

--- telemetria.cpp
+++ telemetria.cpp
@@ -4,19 +4,60 @@
 #include "ff.h"
 #include "sd_manager.h"
 #include "mpuObject.h"
+#include "hardware/gpio.h"
+#include "hardware/irq.h"
 
 mpu9250 mpu(100);  //Creates an mpu object
 
+//Interrupciones
+const uint interruptPin = 2;
+
+//Variables para la SD
+FRESULT fr;
+FATFS fs;
+FIL fil;
+int ret;
+char buf[100];
+char filename[] = "acelData.txt";
+char str[100];
+
+//Funciones
+void sys_init(void);
+void sys_stop();
+
 int main() {
-    stdio_init_all();
-
-    sleep_ms(2000);
-
-    printf("Hello, MPU9250! Reading raw data from registers via SPI...\n");
-
-    while (1)
+    sys_init();//Inicialización del sistema
+    sd_openfileW(fr, &fil, filename);
+    while(1)
     {
         mpu.updateAngles();  //Uses the object to calculate the angles
         mpu.printData();     //Uses the object to print the data
+        sprintf(str, "%d,%d,%d\n", mpu.acceleration[0], mpu.acceleration[1], mpu.acceleration[2]);
+        //printf(str);
+        sd_writefile(ret, &fil, str);
+        if(gpio_get(interruptPin) == 1){ //Haciendo polling al botón de parar de guardar los datos (PENDIENTE POR INTERRUPCION)
+            sys_stop();
+        }   
     }
 }
+
+void sys_init(void){
+    stdio_init_all();//Inicializar I/O de C
+    gpio_init(interruptPin);
+    gpio_set_dir(interruptPin, GPIO_IN);
+
+    //Delay
+    sleep_ms(6000);
+    //Inicializar todo el apartado de la SD
+    initialize_sd();
+    mount_drive(fr, &fs);
+    
+}
+
+void sys_stop(){
+    sd_closefile(fr, &fil);
+    gpio_put(PICO_DEFAULT_LED_PIN, 1);
+    printf("Datos escritos");
+    while (1);
+}
+
