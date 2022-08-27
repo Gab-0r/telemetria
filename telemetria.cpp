08df26e7d17db262ace3cc94b2b12851a29757ac
Gabriel <jgabriel.orozco@udea.edu.co>
2022-08-27 07:00:15 -0500

    Se envían los datos de la IMU por serial (Acelerometro y giroscopio). Pendiente el magnetometro.

--- telemetria.cpp
+++ telemetria.cpp
@@ -3,7 +3,20 @@
 #include "sd_card.h"
 #include "ff.h"
 #include "sd_manager.h"
+#include "mpuObject.h"
+
+mpu9250 mpu(100);  //Creates an mpu object
 
 int main() {
-    
+    stdio_init_all();
+
+    sleep_ms(2000);
+
+    printf("Hello, MPU9250! Reading raw data from registers via SPI...\n");
+
+    while (1)
+    {
+        mpu.updateAngles();  //Uses the object to calculate the angles
+        mpu.printData();     //Uses the object to print the data
+    }
 }
